2dc497d01f653bfb397e3fb2ffa709d7e4adb369
JustinMonty20 <montgomerycodepro@gmail.com>
2025-04-21 20:45:12 -0400

    connector alpha

--- internal/gis/connector.go
+++ internal/gis/connector.go
@@ -1,1 +1,13 @@
 package gis
+
+import (
+  "context"
+  "github.com/JustinMonty20/landman/internal/models"
+)
+// first iteration here. 
+// thinking that each county connector that implements this will end up having the timeing frequency to update. 
+// each county will have its diff logic and then transform the information// into our simplified type.
+type Connector interface {
+  GetData(ctx context.Context) ([]models.Parcel, error)
+  Diff(ctx context.Context) ([]models.Parcel, error)
+}
