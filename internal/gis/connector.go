6e6b5ca76daccb9377e7ddaecd9bb521f1770109
JustinMonty20 <montgomerycodepro@gmail.com>
2025-04-21 21:05:50 -0400

    multi line comment

--- internal/gis/connector.go
+++ internal/gis/connector.go
@@ -4,9 +4,13 @@ import (
   "context"
   "github.com/JustinMonty20/landman/internal/models"
 )
-// first iteration here. 
-// thinking that each county connector that implements this will end up having the timeing frequency to update. 
-// each county will have its diff logic and then transform the information// into our simplified type.
+
+/*
+  first iteration here
+  each county connector that implements this will end having their own tim  ing frequency to update. Information like that doesn't need to go there   right now.
+  
+  Each county has its own diffing logic and then from there it will transf  orm the information back into our simplified types.
+*/
 type Connector interface {
   GetData(ctx context.Context) ([]models.Parcel, error)
   Diff(ctx context.Context) ([]models.Parcel, error)
