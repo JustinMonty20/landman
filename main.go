08a36e8eca5e970bdc53c50cf7463bae67f5403e
JustinMonty20 <montgomerycodepro@gmail.com>
2025-06-08 16:55:44 -0400

    integration tests of all my pieces so far

--- main.go
+++ main.go
@@ -2,8 +2,37 @@ package main
 
 import (
   "fmt"
+  "time"
+	"github.com/JustinMonty20/landman/internal/gis/counties"
 )
 
-func main () {
-  fmt.Println("All good within some function")
+func main() {
+	mainClient := counties.NewHttpClient()
+	rateLimitedClient := counties.NewRateLimitedClient(mainClient, 1)
+
+	ucc, err := counties.NewUnionCountyConnector(
+		"https://ucwater.unioncountync.gov/arcgis/rest/services/GoMaps/UnionGoMaps/MapServer/10/query",
+		"Union",
+		"Union County Parcel Data",
+		rateLimitedClient,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	params := counties.UCArcGisParams{
+    Where: "1=1",
+		ReturnCountOnly: true,
+    Format: "geojson",
+	}
+
+	count, err := ucc.QueryTotalParcels(params)
+  if err != nil {
+    panic(err) 
+  }
+
+  fmt.Printf("Total parcels as of %v: %d", time.Now().UTC(), count) 
+
+  // I would initialize my batching process here based on the the count. 
 }
